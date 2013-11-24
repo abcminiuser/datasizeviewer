1a8f0795f20d9b2c77b974162c3b3a6b251112ab
Dean Camera <dean@fourwalledcubicle.com>
2013-11-24 01:33:56 +0100

    Compile the filter RegEx to speed up filtering.

--- DataSizeViewerExt/src/DataSizeViewer.xaml.cs
+++ DataSizeViewerExt/src/DataSizeViewer.xaml.cs
@@ -124,7 +124,7 @@ namespace FourWalledCubicle.DataSizeViewerExt
             try
             {
                 if (DataSizeViewerPackage.Options.UseRegExFiltering)
-                    isMatch = (new Regex(mFilterString)).IsMatch(currentItem.Name);
+                    isMatch = (new Regex(mFilterString, RegexOptions.Compiled)).IsMatch(currentItem.Name);
                 else
                     isMatch = currentItem.Name.Contains(mFilterString);
             }
